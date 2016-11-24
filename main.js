a91d195d1dbf0b75d046a3c644c570e4743c21f6
Annie Kang <kang.annie@gmail.com>
2016-11-24 14:45:58 -0800

    uploading updated files for 'user can search for items.' #1

--- main.js
+++ main.js
@@ -31,21 +31,112 @@ var jsProducts = [
     brand: "Sony",
     price: "$27.27",
     image: "https://images-na.ssl-images-amazon.com/images/I/61p1oDWAPUL._SL1200_.jpg"},
+  { description: "Wilson Men's 2015 Ultra Complete Package Golf Set, Standard",
+    brand: "Wilson",
+    price: "$155.51",
+    image: "https://images-na.ssl-images-amazon.com/images/I/517bv3TTfRL._SL1000_.jpg"},
+  { description: "Skullcandy Hesh 2 Bluetooth Wireless Headphones with Mic, Black",
+    brand: "Skullcandy",
+    price: "$49.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/7197WSYscIL._SL1500_.jpg"},
+  { description: "Samsung 3.5mm Stereo Headset for Galaxy S5, S4, S3, Note - Non-Retail Packaging - White",
+    brand: "Samsung",
+    price: "$4.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/31tZplZhyEL.jpg"},
+  { description: "Beats Studio Wireless Over-Ear Headphone - Titanium",
+    brand: "Beats",
+    price: "$219.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/71Hgw5UEEXL._SL1500_.jpg"},
+  { description: "Sony MDRXB950BT/B Extra Bass Bluetooth Headphones (Black)",
+    brand: "Sony",
+    price: "$88.00",
+    image: "https://images-na.ssl-images-amazon.com/images/I/61WaUPGzeKL._SL1200_.jpg"},
+  { description: "Beats Solo2 Wireless On-Ear Headphone - Black",
+    brand: "Bose",
+    price: "$149.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/61b8ZEFmCJL._SL1500_.jpg"},
+  { description: "Bose QuietComfort 35 Wireless Headphones, Noise Cancelling - Black",
+    brand: "Bose",
+    price: "$349.00",
+    image: "https://images-na.ssl-images-amazon.com/images/I/61QwytXOcxL._SL1500_.jpg"},
+  { description: "Beats Solo3 Wireless On-Ear Headphone - Rose Gold",
+    brand: "Beats",
+    price: "$249.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/41YByxxpv0L.jpg"},
+  { description: "Sennheiser RS120 On-Ear Wireless RF Headphones with Charging Dock",
+    brand: "Sennheiser",
+    price: "$58.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/81NzuWuye9L._SL1500_.jpg"},
+  { description: "Bose SoundTrue around-ear headphones II - Apple devices, Charcoal",
+    brand: "Bose",
+    price: "$99.00",
+    image: "https://images-na.ssl-images-amazon.com/images/I/61l0i5ugsfL._SL1500_.jpg"},
+  { description: "Amazon Fire TV | Streaming Media Player",
+    brand: "Amazon",
+    price: "$74.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/61jPxT1VThL._SL1000_.jpg"},
+  { description: "Amazon Echo - Black",
+    brand: "Amazon",
+    price: "$139.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/61kJf5cN0gL._SL1000_.jpg"},
+  { description: "Roku Streaming Stick (3600R)",
+    brand: "Roku",
+    price: "$39.99",
+    image: "https://images-na.ssl-images-amazon.com/images/I/61bqlBDRRML._SL1500_.jpg"},
+
+
 ];
 
+var jsLogo = document.getElementById("amazon"); //issue2
 var jsSearch = document.getElementById("search");
-var jsText;
-var jsResults;
+var jsText = document.getElementById("text");
+var jsResults = document.getElementById("products");
 var jsResult;
+var jsString;
+var jsResultString=document.getElementById("outcome");
+var jsResultStringText;
+
+//when page loads or amazon logo is clicked...
+function refresh() { //issue2
+  //event.preventDefault(); //issue2
+  jsText = document.getElementById("text"); //issue2
+  //empty(jsText); //issue2
+  empty(jsResultString); //issue2
+  empty(jsResults); //issue2
+  for (var i = 0; i < 9; i++) { //issue2
+    var jsRandom = Math.floor(Math.random() * jsProducts.length); //issue2
+    var jsImage = renderLoad(jsRandom, jsProducts[jsRandom]); //issue2
+    jsResults = document.getElementById("products"); //issue2
+    jsResults.appendChild(jsImage); //issue2
+  } //issue2
+  jsString = "Items to consider"; //issue2
+  jsResultStringText = document.createTextNode(jsString); //issue2
+  jsResultString.appendChild(jsResultStringText); //issue2
+} //issue2
+
+jsLogo.addEventListener("click", refresh); //issue2
+window.addEventListener("load", refresh); //issue2
+
+function renderLoad(i, product) { //issue2
+  var jsImage = document.createElement("img"); //issue2
+  jsImage.classList.add("image", i); //issue2
+  jsImage.setAttribute("src", product.image); //issue2
+  return jsImage;
+}
+
 
 function listener(event) {
   event.preventDefault();
   jsText = document.getElementById("text").value; //searched text
 
   //do nothing if search is empty
-  if (!jsText.trim()) return;
+  if (!jsText.trim()) {
+  // pass in event?
+  //  refresh(); //issue2
+    return;
+  }
 
-  jsResults = document.getElementById("products");
+  empty(jsResultString);
   empty(jsResults);
 
   /*1. compare search text with each array item in jsProducts
@@ -56,28 +147,26 @@ function listener(event) {
   */
   for (var i = 0; i < jsProducts.length; i++) {
     if (search(jsProducts[i].description, jsText)) {
-      jsResult = renderResult(jsProducts[i]);
+      jsResult = renderResult(i,jsProducts[i]);
       jsResults = document.getElementById("products");
       jsResults.appendChild(jsResult);
     }
   }
+
     if (jsResults.firstChild) {
-      var jsString = "Showing most relevant results. See all results for \"" + jsText + "\".";
-      var jsResultString = document.getElementById("outcome");
-      var jsResultStringText = document.createTextNode(jsString);
+      jsString = "Showing most relevant results. See all results for \"" + jsText + "\".";
+      jsResultStringText = document.createTextNode(jsString);
       jsResultString.appendChild(jsResultStringText);
     }
     else {
-      var jsString2 = "Your search \"" + jsText + "\" did not match any products.";
-      var jsResultString2 = document.getElementById("outcome");
-      var jsResultStringText2 = document.createTextNode(jsString2);
-      jsResultString2.appendChild(jsResultStringText2);
+      jsString = "Your search \"" + jsText + "\" did not match any products.";
+      jsResultStringText = document.createTextNode(jsString);
+      jsResultString.appendChild(jsResultStringText);
     }
   //page is refreshed with same results if search button is pressed again
 }
 
 jsSearch.addEventListener("submit", listener);
-//add EventListener on keying return after typing in a search
 
 //empties out any elements within element with "product" ID
 function empty(jsResults) {
@@ -87,6 +176,7 @@ function empty(jsResults) {
 }
 
 //returns whether searched text matches with jsProducts[i]
+//search function checks to see that each word of the search is contained in an item description
 function search(description, jsText) {
   jsText = jsText.toLowerCase();
   jsText = jsText.trim();
@@ -107,10 +197,10 @@ function splitString(stringToSplit, separator) {
   return arrayOfStrings;
 }
 
-function renderResult(product) {
+function renderResult(i, product) {
 /*
 <div class="product">
-  <div class="ImageDiv"><img class="png" src="link.com"/></div>
+  <img class="image" src="link.com"/>
   <div class="info">
     <div class="description"></div>
     <div class="brand"></div>
@@ -120,38 +210,38 @@ function renderResult(product) {
 */
 
 var jsProduct2 = document.createElement("div");
-jsProduct2.classList.add("product");
-
-var jsImageDiv = document.createElement("div");
-jsImageDiv.classList.add("image");
+jsProduct2.classList.add("product", i);
 
 var jsImage = document.createElement("img");
-jsImage.classList.add("png");
+jsImage.classList.add("image", i);
 jsImage.setAttribute("src", product.image);
 
-jsImageDiv.appendChild(jsImage);
-
 var jsInfo = document.createElement("div");
-jsInfo.classList.add("info");
+jsInfo.classList.add("info", i);
 
 var jsDescription = document.createElement("div");
-jsDescription.classList.add("description");
+jsDescription.classList.add("description", i);
 jsDescription.textContent = product.description;
 
 var jsBrand = document.createElement("div");
-jsBrand.classList.add("brand");
-jsBrand.textContent = product.brand;
+jsBrand.classList.add("brand", i);
+jsBrand.textContent = "by " + product.brand;
 
 var jsPrice = document.createElement("div");
-jsPrice.classList.add("price");
+jsPrice.classList.add("price", i);
 jsPrice.textContent = product.price;
 
 jsInfo.appendChild(jsDescription);
 jsInfo.appendChild(jsBrand);
 jsInfo.appendChild(jsPrice);
 
-jsProduct2.appendChild(jsImageDiv);
+jsProduct2.appendChild(jsImage);
 jsProduct2.appendChild(jsInfo);
 
 return jsProduct2;
 }
+
+/*
+eventListener on clicking products
+event.target
+*/
